2c03c4efe7ed3ce1b3903c84f80a85832f11eed2
jonghwa oh <ojonghwa@naver.com>
2019-05-15 01:36:07 +0900

    Bookmark Service

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError from exc
+
 #raise ImportError("Couldn't import Django.") from exc
 
     execute_from_command_line(sys.argv)
